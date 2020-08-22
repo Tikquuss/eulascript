0c114397981e31a3e3eccce1045d2fc655fb84bb
Tikquuss <pascalnotsawo@gmail.com>
2020-08-22 06:21:39 +0200

    add logistic regression models

--- eula.py
+++ eula.py
@@ -4,8 +4,10 @@ import csv
 import pandas as pd 
 import validators
 import ktrain
+from threading import Thread
 
 from utils import path_leaf, is_supported, get_content, get_ktrain_predict_method, download
+from utils import convert_to_clauses
 
 def get_parser():
     """
@@ -24,36 +26,57 @@ def get_parser():
     
     parser.add_argument("--cache_path", type=str, default="cache", 
                         help="folder in which the models will be stored temporarily")
+
+    parser.add_argument("--logistic_regression", type=str, default="", 
+                        help="bag_of_word or tf_idf or bert")
+
+
     
     return parser
   
+def thread_target(eula_file, predict_method) :
+
+    print("============ eula_file : ", eula_file, " ============")
+          
+    """
+    clause_list = get_content(eula_file)
+    clause_dic = convert_to_clauses(clause_list)
+    """
+    _, _, clause_dic, _ = get_content(eula_file)
+    
+    clause_list = list(clause_dic.values())
+    clauses_key = list(clause_dic.keys())
+
+    probabilities = predict_method(clause_list)
+    labels = [1 if y >= 0.5 else 0 for y in probabilities]
+
+    file_name = path_leaf(path = eula_file)
+    file_name, _ = os.path.splitext(file_name) 
+
+    csv_file = file_name+".csv"
+        
+    if os.path.isfile(csv_file):
+        i = 1
+        while os.path.isfile(file_name+'.'+str(i)+".csv"):
+            i += 1
+        csv_file = file_name+'.'+str(i)+'.csv'
+
+    print("============ csv_file : ", csv_file, " ============")
+          
+    pd.DataFrame(zip(clauses_key, clause_list, labels, probabilities)).to_csv(csv_file, header= ["clauses_key" ,"clauses", "labels", "probabilities"])
+
+
 def main(params):
 
-    predict_method = get_ktrain_predict_method(ktrain_predictor = ktrain.load_predictor(params.model_folder))      
+    if a :
+        predict_method = get_ktrain_predict_method(ktrain_predictor = ktrain.load_predictor(params.model_folder))  
+    else :
+        from logistic_regression import get_predict_method
+        predict_method = get_predict_method(params.logistic_regression)
+        
     
     for eula_file in params.eula_files :
-
-        print("============ eula_file : ", eula_file, " ============")
-        
-        clause_list = get_content(eula_file)
-        
-        probabilities = predict_method(clause_list)
-        labels = [1 if y >= 0.5 else 0 for y in probabilities]
-
-        file_name = path_leaf(path = eula_file)
-        file_name, _ = os.path.splitext(file_name) 
-
-        csv_file = file_name+".csv"
-        
-        if os.path.isfile(csv_file):
-            i = 1
-            while os.path.isfile(file_name+'.'+str(i)+".csv"):
-                i += 1
-            csv_file = file_name+'.'+str(i)+'.csv'
-
-        print("============ csv_file : ", csv_file, " ============")
-        
-        pd.DataFrame(zip(clause_list, labels, probabilities)).to_csv(csv_file, header= ["clauses", "labels", "probabilities"])
+      Thread(target = thread_target,  kwargs={"eula_file" : eula_file, "predict_method" : predict_method}).start()
 
 if __name__ == '__main__':
 
@@ -62,19 +85,24 @@ if __name__ == '__main__':
     params = parser.parse_args()
 
     # check parameters
-    assert params.model_folder
+    #assert params.model_folder
     if params.model_folder.startswith("http:") or params.model_folder.startswith("https:") :
         assert validators.url(params.model_folder)
         url = params.model_folder.split("/")
         model_name = url[-1]
         base_url = '/'.join(url[:len(url)-1])+"/"
-        to_load = {
+        to_load = { 
                 model_name : ["tf_model.preproc", "config.json" , "tf_model.h5"]
         }
         download(output_path = params.cache_path, to_load = to_load, base_url = base_url)
         params.model_folder = os.path.join(params.cache_path, model_name)
     else :
-        assert os.path.isdir(params.model_folder), "model folder path not found"
+        a = os.path.isdir(params.model_folder)
+        b = params.logistic_regression in ["bag_of_word","tf_idf","bert"]
+        assert a or b, "model folder path not found"
+        if a :
+            assert all([os.path.isfile(os.path.join(params.model_folder, f)) for f in ["tf_model.preproc", "config.json" , "tf_model.h5"]])
+
 
     eula_files = params.path_to_eula.split(",")
     assert eula_files
